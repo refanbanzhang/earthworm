30ddbe698df79f76d1d68d40d6c6708a2c230b7d
refanbanzhang <gaoqiang19514@163.com>
2024-04-21 19:35:43 +0800

    refactor: read code

--- apps/client/composables/user/gameMode.ts
+++ apps/client/composables/user/gameMode.ts
@@ -13,13 +13,28 @@ export const gameModeLabels: { [key in GameMode]: string } = {
 const GameModeKey = "gameMode";
 const currentGameMode = ref<GameMode>(GameMode.ChineseToEnglish);
 
+function getFromLocalStorage(key: string) {
+  const data = localStorage.getItem(key)
+
+  if (data === null) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error parsing data from localStorage key "${key}": ${error}`);
+    return null;
+  }
+}
+
 function loadCache() {
   const mode = getStore() || currentGameMode.value;
   currentGameMode.value = mode;
 }
 
 function getStore() {
-  return localStorage.getItem(GameModeKey) as GameMode;
+  return getFromLocalStorage(GameModeKey) as GameMode;
 }
 
 function setStore(value: GameMode) {
@@ -30,6 +45,7 @@ loadCache();
 
 export function useGameMode() {
   function getGameModeOptions() {
+    // 对象转数组
     return Object.entries(gameModeLabels).map(([key, value]) => {
       return {
         label: value,
@@ -40,6 +56,7 @@ export function useGameMode() {
 
   function toggleGameMode(mode: GameMode) {
     currentGameMode.value = mode;
+    // 缓存到本地
     setStore(mode);
   }
 
