b83103276dc83401c71078f0b898b7feaa825fc1
refanbanzhang <gaoqiang19514@163.com>
2024-06-28 20:26:29 +0800

    feat: 学习

--- apps/client/components/Navbar.vue
+++ apps/client/components/Navbar.vue
@@ -1,46 +1,27 @@
 <template>
-  <header
-    class="w-full px-5 font-customFont transition-all duration-300 ease-linear"
-    :class="{
-      'sticky top-0 z-10': isStickyNavBar,
-      'glass bg-gradient-to-r from-transparent via-white/10 to-transparent shadow-md':
-        isStickyNavBar && isScrolled,
-    }"
-  >
+  <header class="w-full px-5 font-customFont transition-all duration-300 ease-linear" :class="{
+    'sticky top-0 z-10': isStickyNavBar,
+    'glass bg-gradient-to-r from-transparent via-white/10 to-transparent shadow-md':
+      isStickyNavBar && isScrolled,
+  }">
     <div class="mx-auto max-w-screen-xl">
       <div class="flex h-16 items-center justify-between">
         <div class="flex flex-1 items-center justify-between">
           <NuxtLink to="/">
             <div class="logo flex items-center">
-              <img
-                width="48"
-                height="48"
-                class="mr-6 hidden overflow-hidden rounded-md md:block"
-                src="/logo.png"
-                alt="earth-worm-logo"
-              />
+              <img width="48" height="48" class="mr-6 hidden overflow-hidden rounded-md md:block" src="/logo.png"
+                alt="earth-worm-logo" />
               <h1 class="text-wrap text-2xl font-extrabold leading-normal dark:text-white">
                 Earthworm
               </h1>
             </div>
           </NuxtLink>
 
-          <nav
-            v-if="route.path === '/' && !isAuthenticated()"
-            aria-label="Global"
-            class="hidden md:block"
-          >
+          <nav v-if="route.path === '/' && !isAuthenticated()" aria-label="Global" class="hidden md:block">
             <ul class="flex items-center text-base">
-              <li
-                class="px-4"
-                v-for="(optItem, optIndex) in HEADER_OPTIONS"
-                :key="optIndex"
-              >
-                <a
-                  class="text-nowrap hover:text-purple-600 dark:text-white dark:hover:text-purple-400"
-                  :href="optItem.href"
-                  :target="optItem.target ?? optItem.target"
-                >
+              <li class="px-4" v-for="(optItem, optIndex) in HEADER_OPTIONS" :key="optIndex">
+                <a class="text-nowrap hover:text-purple-600 dark:text-white dark:hover:text-purple-400"
+                  :href="optItem.href" :target="optItem.target ?? optItem.target">
                   {{ optItem.name }}
                 </a>
               </li>
@@ -50,27 +31,17 @@
 
         <div class="flex items-center">
           <!-- 显示用户信息 -->
-          <div
-            v-if="isAuthenticated()"
-            class="logged-in flex items-center"
-          >
+          <div v-if="isAuthenticated()" class="logged-in flex items-center">
             <div
               class="h-8 w-8 cursor-pointer overflow-hidden rounded-full bg-gray-300 transition-all hover:scale-125 hover:opacity-90 dark:bg-gray-700"
-              @click="handleShowUserMenu"
-            >
-              <img
-                class="h-full object-cover"
-                :src="userStore.userInfo?.picture!"
-              />
+              @click="handleShowUserMenu">
+              <img class="h-full object-cover" :src="userStore.userInfo?.picture!" />
             </div>
           </div>
           <!-- 登录/注册 -->
-          <button
-            v-else
-            aria-label="Login"
+          <button v-else aria-label="Login"
             class="btn btn-sm mr-1 border-none bg-purple-500 text-white shadow-md hover:bg-purple-600 focus:outline-none"
-            @click="signIn()"
-          >
+            @click="signIn()">
             登录
           </button>
         </div>
@@ -78,16 +49,8 @@
     </div>
   </header>
 
-  <UserMenu
-    v-model:open="isOpenUserMenu"
-    @logout="handleLogout"
-  />
-  <MainMessageBox
-    v-model:show-modal="isShowModal"
-    content="是否确认退出登录？"
-    confirm-btn-text="确认"
-    @confirm="signOut"
-  />
+  <UserMenu v-model:open="isOpenUserMenu" @logout="handleLogout" />
+  <MainMessageBox v-model:show-modal="isShowModal" content="是否确认退出登录？" confirm-btn-text="确认" @confirm="signOut" />
 </template>
 
 <script setup lang="ts">
@@ -102,6 +65,7 @@ import { useUserStore } from "~/store/user";
 const runtimeConfig = useRuntimeConfig();
 
 const route = useRoute();
+// store不能解构是否，否则会失去响应效果
 const userStore = useUserStore();
 const { y } = useWindowScroll();
 
